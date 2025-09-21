7c9ef7bd5d2de513a6d1078bc50ea93195cd0ae3
Aqib Javaid <aqibjavaidabbasi@gmail.com>
2025-09-21 20:12:09 +0500

    Added Tagmanger scripts

--- app/layout.js
+++ app/layout.js
@@ -121,6 +121,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/panda-logo-black.png" />
+        {/* <!-- Google Tag Manager --> */}
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TZG593F9');</script>
+        {/* <!-- End Google Tag Manager --> */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#090d11]`}
@@ -132,6 +139,10 @@ export default function RootLayout({ children }) {
           <Popup />
         </PopupProvider>
       </body>
+      {/* <!-- Google Tag Manager (noscript) --> */}
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZG593F9"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      {/* <!-- End Google Tag Manager (noscript) --> */}
     </html>
   );
 }
